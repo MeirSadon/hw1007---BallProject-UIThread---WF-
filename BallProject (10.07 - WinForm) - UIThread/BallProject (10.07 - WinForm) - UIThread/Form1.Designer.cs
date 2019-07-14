63c9c97b8eef58140ec069e80b34eba835b6b05d
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-07-14 21:05:29 +0300

    Add files via upload

---
+++ BallProject (10.07 - WinForm) - UIThread/BallProject (10.07 - WinForm) - UIThread/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BallProject__10._07___WinForm____UIThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CreateBallBtn = new System.Windows.Forms.Button();
+            this.BallImg = new System.Windows.Forms.PictureBox();
+            this.RemoveBallsImg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.BallImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CreateBallBtn
+            // 
+            this.CreateBallBtn.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.CreateBallBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.CreateBallBtn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.CreateBallBtn.Location = new System.Drawing.Point(0, 0);
+            this.CreateBallBtn.Name = "CreateBallBtn";
+            this.CreateBallBtn.Size = new System.Drawing.Size(403, 43);
+            this.CreateBallBtn.TabIndex = 0;
+            this.CreateBallBtn.Text = "Create Ball";
+            this.CreateBallBtn.UseVisualStyleBackColor = false;
+            this.CreateBallBtn.Click += new System.EventHandler(this.AddBallBtn_Click);
+            // 
+            // BallImg
+            // 
+            this.BallImg.Image = ((System.Drawing.Image)(resources.GetObject("BallImg.Image")));
+            this.BallImg.InitialImage = ((System.Drawing.Image)(resources.GetObject("BallImg.InitialImage")));
+            this.BallImg.Location = new System.Drawing.Point(386, 198);
+            this.BallImg.Name = "BallImg";
+            this.BallImg.Size = new System.Drawing.Size(70, 71);
+            this.BallImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BallImg.TabIndex = 1;
+            this.BallImg.TabStop = false;
+            // 
+            // RemoveBallsImg
+            // 
+            this.RemoveBallsImg.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.RemoveBallsImg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.RemoveBallsImg.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.RemoveBallsImg.Location = new System.Drawing.Point(398, 0);
+            this.RemoveBallsImg.Name = "RemoveBallsImg";
+            this.RemoveBallsImg.Size = new System.Drawing.Size(402, 43);
+            this.RemoveBallsImg.TabIndex = 2;
+            this.RemoveBallsImg.Text = "Remove Balls";
+            this.RemoveBallsImg.UseVisualStyleBackColor = false;
+            this.RemoveBallsImg.Click += new System.EventHandler(this.RemoveBallsImg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RemoveBallsImg);
+            this.Controls.Add(this.BallImg);
+            this.Controls.Add(this.CreateBallBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.BallImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateBallBtn;
+        private System.Windows.Forms.PictureBox BallImg;
+        private System.Windows.Forms.Button RemoveBallsImg;
+    }
+}
+
---
+++ BallProject (10.07 - WinForm) - UIThread/BallProject (10.07 - WinForm) - UIThread/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallProject__10._07___WinForm____UIThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BallImg.Visible = false;
+        }
+        int dx = -1;
+        int dy = -1;
+
+        private void AddBallBtn_Click(object sender, EventArgs e)
+        {
+            Action a = () => { BallImg.Location = new Point(BallImg.Location.X + dx, BallImg.Location.Y + dy); };
+            BallImg.Visible = true;
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (BallImg.Location.X + dx <= 0)
+                        dx = 1;
+                    if (BallImg.Location.X + dx >= ClientSize.Width - BallImg.Width)
+                        dx = -1;
+                    if (BallImg.Location.Y + dy <= 0 + CreateBallBtn.Height)
+                        dy = 1;
+                    if (BallImg.Location.Y + dy >= ClientSize.Height - BallImg.Height)
+                        dy = -1;
+                    BallImg.BeginInvoke(a);
+                    Thread.Sleep(10);
+                }
+            });
+        }
+
+        private void RemoveBallsImg_Click(object sender, EventArgs e)
+        {
+            BallImg.Visible = false;
+        }
+    }
+}
